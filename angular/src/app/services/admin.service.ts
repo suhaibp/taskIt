7772eeaadf4352118c9a7d0cb24fcc5b1ce4cb86
ScottMachlovski <prazad.manu@gmail.com>
2018-03-06 09:09:38 +0530

    basic count queries & design

--- angular/src/app/services/admin.service.ts
+++ angular/src/app/services/admin.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Config } from '../config/config';
 
 @Injectable()
 export class AdminService {
+  serviceUrl: string;
+  constructor(private http: Http, private config: Config) {
+    this.serviceUrl = config.siteUrl + '/admin/';
+   }
 
-  constructor() { }
 
+   setHeader() {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return (headers);
+  }
+  //  ---------------------------------Start-------------------------------------------
+  // Function      : getCountsforAdminDashboard
+  // Params        : 
+  // Returns       : 
+  // Author        : Manu Prasad
+  // Date          : 02-03-2018
+  // Last Modified : 02-03-2018, Jooshifa 
+  // Desc          : for add industry in admin master settings
+
+
+  getCountsforAdminDashboard() {
+    let h = this.setHeader();
+    return this.http.post(this.serviceUrl + "/create", { headers: h })
+      .map(res => res.json());
+
+  }
 }
--- routes/super_admin.js
+++ routes/super_admin.js
@@ -3,23 +3,43 @@ var router = express.Router();
 const Sequelize = require('sequelize');
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var login = require('../models/login');
+// var login = require('../models/login');
 
-const student = models.student.build({
-  name: "Asif",
-  rollnumber: 123
-})
-
+// const student = models.student.build({
+//   name: "Asif",
+//   rollnumber: 123
+// })
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 var returnRouter = function (io) {
 router.post('/create', function(req, res) {
-    sequelize.query("CALL getallstudent();").then(myTableRows => {
-      console.log(myTableRows.Result)
+    var userCount;
+    var cmpCount;
+    var projectCount;
+    sequelize.query("select count(*) from tbl_role").spread(myTableRows => {
+      // res.json(myTableRows)
+      userCount = myTableRows[0].count;
     })
-
+    sequelize.query("select count(*) from tbl_team").spread(myTableRows => {
+      // res.json(myTableRows)
+      cmpCount = myTableRows[0].count;
+    })
+    sequelize.query("select count(*) from tbl_public_holiday").spread(myTableRows => {
+      // res.json(myTableRows)
+      projectCount = myTableRows[0].count;
+    })
     // student.save().then(function(newStudent){
     //   console.log(newStudent);
     // })
+    res.json({
+      users:userCount,
+      companies:cmpCount,
+      projects:projectCount
+    })
   });
 
   router.get('/:user_id/destroy', function(req, res) {
